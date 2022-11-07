5dbc3168ae0285f1e1fa44ba2501dc3897df4709
xma12 <xma12@slb.com>
2022-11-07 17:20:22 +0800

    test(ziyu and xuehua): add to do service create fail test

--- ToDoFrontEnd/src/app/service/todo.service.spec.ts
+++ ToDoFrontEnd/src/app/service/todo.service.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { throwError } from 'rxjs';
 import { ToDoItem } from '../model/ToDoItem';
 import { TodoStoreService } from './todo-store.service';
 import { TodoService } from './todo.service';
@@ -28,6 +29,7 @@ describe('TodoService', () => {
   it('should create todoItem via mockHttp post', () => {
     // given
     const todoItem = new ToDoItem(8,'title','decription', true);
+    
 
     // when
     service.create(todoItem);
@@ -35,6 +37,18 @@ describe('TodoService', () => {
     //then
     expect(httpClientSpy.post).toHaveBeenCalledWith(
       'https://635fc244ca0fe3c21aa3d012.mockapi.io/api/todos', todoItem)
+    
+  });
+  it('should response err when create fail', () => {
+    // given
+    const todoItem = new ToDoItem(8,'title','decription', true);
+    httpClientSpy.post.and.returnValue(throwError(()=>({errorMessage:"Created failed"})));
+
+    // when
+    service.create(todoItem);
+
+    //then
+    expect(service.errorMessage).toEqual("Created failed");
     
   });
 });
