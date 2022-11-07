10ab505ef4594a3cb32231147db720f052d34ed5
zwang <zwang91@slb.com>
2022-11-07 16:52:53 +0800

    test (Ziyu & Xuehua): service create via httpclient post

--- ToDoFrontEnd/src/app/service/todo.service.spec.ts
+++ ToDoFrontEnd/src/app/service/todo.service.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { ToDoItem } from '../model/ToDoItem';
 import { TodoStoreService } from './todo-store.service';
 import { TodoService } from './todo.service';
 
@@ -6,8 +8,10 @@ describe('TodoService', () => {
 
   let service: TodoService;
   let todoStoreService: TodoStoreService;
+  let httpClient: any;
 
   beforeEach(() => {
+    httpClient = jasmine.createSpyObj('HttpClient', ['post']);
     todoStoreService = new TodoStoreService();
     TestBed.configureTestingModule({});
     service = TestBed.inject(TodoService);
@@ -15,5 +19,18 @@ describe('TodoService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should create todoItem via mockHttp post', () => {
+    // given
+    const todoItem = new ToDoItem(8,'title','decription', true);
+
+    // when
+    service.create(todoItem);
+
+    //then
+    expect(httpClient.post).toHaveBeenCalledWith(
+      'https://635fc244ca0fe3c21aa3d012.mockapi.io/api/todos', todoItem)
+    
   });
 });
